4c7febd599f22f418f6f6c53958eaaf048337740
sekibaalamark <markmajje256@gmail.com>
2025-08-01 12:22:47 +0300

    adjusted the life of jwt for development

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,3 +132,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+}
