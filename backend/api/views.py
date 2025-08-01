9624000dcd7425ae5b7c7f2417a8045fbd31db5e
sekibaalamark <markmajje256@gmail.com>
2025-08-01 10:47:12 +0300

    created a user registration view

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserRegistrationSerializer
+from .serializers import *
 
 class UserRegistrationView(APIView):
     def post(self, request):
@@ -13,4 +13,18 @@ class UserRegistrationView(APIView):
         if serializer.is_valid():
             user = serializer.save()
             return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data["user"]
+            return Response({
+                "message": "Login successful!",
+                "username": user.username,
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
