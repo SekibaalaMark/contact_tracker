98afcbd9c7a9113165ab3ba98caaaec626a8b9af
sekibaalamark <markmajje256@gmail.com>
2025-08-01 11:52:46 +0300

    registered the login api in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,15 @@
 # users/urls.py
 from django.urls import path
 from .views import *
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # refresh
 ]
