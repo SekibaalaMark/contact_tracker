c36d32efb63c0c23bcf0d841863c36108a6710db
sekibaalamark <markmajje256@gmail.com>
2025-08-01 11:52:32 +0300

    created a login view that returns tokens

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 # users/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework import status
 from .serializers import *
 
 class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
@@ -18,13 +20,31 @@ class UserRegistrationView(APIView):
 
 
 
+# users/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserLoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
 class UserLoginView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data["user"]
+            user = serializer.validated_data['user']
+
+            # Generate tokens
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
             return Response({
                 "message": "Login successful!",
                 "username": user.username,
+                "access": access_token,
+                "refresh": refresh_token,
             }, status=status.HTTP_200_OK)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
